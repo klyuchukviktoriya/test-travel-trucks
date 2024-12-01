4f4693efa4fcd1dd0381acaba0aebc501aa1dfc1
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2024-12-02 00:13:17 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  }
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
 });
