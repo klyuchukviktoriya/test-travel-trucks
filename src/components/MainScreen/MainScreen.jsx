de9bcf37e06ca3357a29d4a5b1a643088b4d7599
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2024-12-02 01:16:32 +0200

    bg-main

--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -1,9 +1,21 @@
 import { NavLink } from "react-router-dom";
 import css from "./MainScreen.module.css";
+import bgHome from "/src/assets/bg-home.jpg";
+
 
 export default function MainScreen() {
+
+const styles = {
+  mainScreen: {
+    backgroundImage: `url(${bgHome})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    padding: "260px 64px",
+    color: "#f7f7f7",
+  },
+};
   return (
-    <div className={css.mainScreen}>
+    <div style={styles.mainScreen}>
       <h1 className={css.title}>Campers of your dreams</h1>
       <h2 className={css.titleText}>
         You can find everything you want in our catalog
